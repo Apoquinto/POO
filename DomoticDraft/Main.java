9088f5d1ba8a42beb8a358bf83a617006bb5af0f
Apoquinto <RCLlanesA@gmail.com>
2020-12-07 18:35:33 -0600

    Test: Compila :D

--- DomoticDraft/Main.java
+++ DomoticDraft/Main.java
@@ -1,32 +1,15 @@
-import java.util.ArrayList;
-
 public class Main{
     
     /** 
      * @param args
      */
     public static void main(String[] args) {
-    InventoryA inventory = new InventoryA();
+    Inventory<Device> inventoryDevice = new Inventory<Device>();
 
-    inventory.createInventory();
-    
     DeviceSpecs espec = new DeviceSpecs(Brand.LG, "Model0", true, false);
     Device device0 = new TV(0, true, "TV", espec, "HD");
     
-    System.out.println(inventory.searchDevice(device0));
+    inventoryDevice.addElement( device0 );
     
     }
 }
-/*
-
- |\/\/\/|  
- |      |  
- |      |  
- | (o)(o)  
- C      _) 
-  | ,___|  
-  |   /    
- /____\    
-/      \
-
-*/
