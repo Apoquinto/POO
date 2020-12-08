d8cbe118173a3ceb73967fc69c27a3876d4e4449
Apoquinto <RCLlanesA@gmail.com>
2020-12-07 18:40:08 -0600

    Test: Test 2 - busqueda

--- DomoticDraft/Main.java
+++ DomoticDraft/Main.java
@@ -9,7 +9,17 @@ public class Main{
     DeviceSpecs espec = new DeviceSpecs(Brand.LG, "Model0", true, false);
     Device device0 = new TV(0, true, "TV", espec, "HD");
     
+    DeviceSpecs espec1 = new DeviceSpecs(Brand.LG, "Model0", true, false);
+    Device device1 = new TV(0, true, "TV", espec1, "LED");
+
+    DeviceSpecs espec2 = new DeviceSpecs(Brand.LG, "Model0", true, false);
+    Device device2 = new TV(0, true, "TV", espec2, "OLED");
+
     inventoryDevice.addElement( device0 );
+    inventoryDevice.addElement( device1 );
+    inventoryDevice.addElement( device2 );
+
+    System.out.println( inventoryDevice.searchElement( device2 ) );
     
     }
 }
